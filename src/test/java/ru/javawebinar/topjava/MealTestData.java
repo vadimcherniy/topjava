1dda504a1c769456b69b940d66be3093b664a0ab
vadimcherniy <vajifrose@gmail.com>
2018-09-24 21:55:08 +0300

    Refactoring

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -15,11 +15,11 @@ public class MealTestData {
     public static final Integer ADMIN_MEAL_1_ID = START_SEQ + 3;
     public static final Integer ADMIN_MEAL_2_ID = START_SEQ + 2;
 
-    public static final Meal ADMIN_MEAL_1 = new Meal(ADMIN_MEAL_1_ID, LocalDateTime.of(2018, Month.SEPTEMBER, 1, 19, 0), "Админ ужин", 1500);
+    public static final Meal ADMIN_MEAL_1 = new Meal(ADMIN_MEAL_1_ID, LocalDateTime.of(2018, Month.SEPTEMBER, 1, 19, 0), "Админ ужин", 1000);
     public static final Meal ADMIN_MEAL_2 = new Meal(ADMIN_MEAL_2_ID, LocalDateTime.of(2018, Month.SEPTEMBER, 1, 9, 0), "Админ завтрак", 500);
 
     public static void assertMatch(Meal actual, Meal expected) {
-        assertThat(actual).isEqualTo(expected);
+        assertThat(actual).isEqualToIgnoringGivenFields(expected, "user");
     }
 
     public static void assertMatch(Iterable<Meal> actual, Meal... expected) {
@@ -27,7 +27,7 @@ public class MealTestData {
     }
 
     public static void assertMatch(Iterable<Meal> actual, Iterable<Meal> expected) {
-        assertThat(actual).usingDefaultComparator().isEqualTo(expected);
+        assertThat(actual).usingElementComparatorIgnoringFields("user").isEqualTo(expected);
     }
 
 }
