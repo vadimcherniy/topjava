ce5b443e0ad670c88d069f9a1d733239a0237f56
Vadym Chornyi <vadym.chornyi@globallogic.com>
2018-10-12 18:21:57 +0300

    Web with REST

--- src/main/java/ru/javawebinar/topjava/web/user/AdminRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/AdminRestController.java
@@ -1,40 +1,37 @@
 package ru.javawebinar.topjava.web.user;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.topjava.model.User;
 
 import java.util.List;
 
-@Controller
+@RestController("/rest/admin/users")
 public class AdminRestController extends AbstractUserController {
 
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<User> getAll() {
         return super.getAll();
     }
 
-    @RequestMapping(params = "id", method = RequestMethod.GET)
-    public User get(Integer id) {
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public User get(@PathVariable("id") Integer id) {
         return super.get(id);
     }
 
-    @Override
     public User create(User user) {
         return super.create(user);
     }
 
-    @Override
-    public void delete(Integer id) {
+    @DeleteMapping(value = "/{id}")
+    public void delete(@PathVariable("id") Integer id) {
         super.delete(id);
     }
 
-    @Override
     public void update(User user, Integer id) {
         super.update(user, id);
     }
 
-    @Override
     public User getByMail(String email) {
         return super.getByMail(email);
     }
